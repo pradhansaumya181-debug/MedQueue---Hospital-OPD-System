f33a493a8675f305daaa6a0bc302c6baf226f9d3
Saumya Pradhan <pradhansaumya181@gmail.com>
2026-07-03 13:07:39 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@
 // server/server.js
 
 require('dotenv').config();
-const app = require('./app');
-const connectDB = require('./config/db');
+const app = require('./src/app');
+const connectDB = require('./src/config/db');
 
 const PORT = process.env.PORT || 5000;
 
