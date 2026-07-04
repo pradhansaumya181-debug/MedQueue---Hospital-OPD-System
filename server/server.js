a20729a5c90ce72fbcaca9ebe33ea8fc68e5d871
Saumya Pradhan <pradhansaumya181@gmail.com>
2026-07-04 09:43:53 +0530

    new added

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const express   = require('express')
 const cors      = require('cors')
-const app2      = require('./src/app')
+const app      = require('./src/app')
 const connectDB = require('./src/config/db')
 
 const PORT = process.env.PORT || 10000
